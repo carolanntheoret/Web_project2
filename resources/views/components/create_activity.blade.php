33158b61ed4416a5b4ffc36127f355e16567e601
Gabriel Rhéaume <gabriel.rheaume1@hotmail.com>
2023-03-13 11:32:20 -0400

    oh shit c'est compliqué, on verra ça un autre jour

--- resources/views/components/create_activity.blade.php
+++ resources/views/components/create_activity.blade.php
@@ -1,3 +1,12 @@
-<form action="" method="post">
-
+<form action="/onverra" method="post">
+    <div>
+        <span>Title :</span>
+        <input name="title" type="text">
+    </div>
+    <div>
+        <span>Description :</span>
+        <input name="description" type="text">
+    </div>
+    <div></div>
+    <div></div>
 </form>
