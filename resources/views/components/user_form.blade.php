a0d85c5920728f6d2ad138f9af6f9d3579cc863f
Gabriel Rhéaume <gabriel.rheaume1@hotmail.com>
2023-03-20 10:51:16 -0400

    create account done ! except dealing with header

--- resources/views/components/user_form.blade.php
+++ resources/views/components/user_form.blade.php
@@ -1,11 +1,13 @@
 <form action="<?= isset($user) ? '/modify-user' : '/create-user' ?>" method="post">
     @csrf
-    <input type="text" name="first_name" value="<?= $user->first_name ?? '' ?>" placeholder="First Name" required>
-    <input type="text" name="last_name" value="<?= $user->last_name ?? '' ?>" placeholder="Last Name" required>
+    <div class="createAccountName">
+        <input type="text" name="first_name" value="<?= $user->first_name ?? '' ?>" placeholder="First Name" required>
+        <input type="text" name="last_name" value="<?= $user->last_name ?? '' ?>" placeholder="Last Name" required>
+    </div>
     <input type="email" name="email" value="<?= $user->email ?? '' ?>" placeholder="Email" required>
     <input type="password" name="password" <?= isset($user) ? '' : 'required' ?> placeholder="Password">
     <input type="password_confirmation" name="password" <?= isset($user) ? '' : 'required' ?> placeholder="Confirm Password">
-    @if ($admin && !isset($user))
+    @if ($admin && strpos(url()->current(), '/admin'))
         <div>
             <span>Role :</span>
             <select name="admin" required>
@@ -17,7 +19,8 @@
     @if (isset($user))
         <input name="id" type="hidden" value="<?= $user->id ?>">
     @endif
-    <div CLASS="submit">
-        <input type="submit" value="CREATE ACCOUNT">
+    <div class="createAccountInput">
+        <input class="submit" type="submit" value="CREATE ACCOUNT">
+        <p class="plus">+</p>
     </div>
 </form>
