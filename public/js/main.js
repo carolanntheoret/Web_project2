633f410c24826327010e4b87fc018ad9bbeecdb5
CSTJ\1546960 <nicosubywrx@gmail.com>
2023-03-24 10:59:05 -0400

    Integration de l'image du bouton modifier

--- public/js/main.js
+++ public/js/main.js
@@ -1,25 +1,46 @@
+$(".listEmployes").hide();
+jQuery(".employes").on("click", function () {
+    jQuery(".listEmployes").show() &
+        $(".listActivities").hide() &
+        $(".listReservations").hide() &
+        $(".listUsers").hide();
+});
 
-$('.listEmployes').hide()
-jQuery('.employes').on('click',function(){
-  jQuery('.listEmployes').toggle() & $('.listActivities').hide() & $('.listReservations').hide() & $('.listUsers').hide();
-})
+$(".listUsers").hide();
+jQuery(".users").on("click", function () {
+    jQuery(".listUsers").show() &
+        $(".listActivities").hide() &
+        $(".listReservations").hide() &
+        $(".listEmployes").hide();
+});
 
-$('.listUsers').hide()
-jQuery('.users').on('click',function(){
-  jQuery('.listUsers').toggle() & $('.listActivities').hide() & $('.listReservations').hide() & $('.listEmployes').hide();
-})
+$(".listActivities").hide();
+jQuery(".activities").on("click", function () {
+    jQuery(".listActivities").show() &
+        $(".listEmployes").hide() &
+        $(".listReservations").hide() &
+        $(".listUsers").hide();
+});
 
-$('.listActivities').hide()
-jQuery('.activities').on('click',function(){
-  jQuery('.listActivities').toggle() & $('.listEmployes').hide() & $('.listReservations').hide() & $('.listUsers').hide();
-})
+jQuery(".reservations").on("click", function () {
+    jQuery(".listReservations").show() &
+        $(".listEmployes").hide() &
+        $(".listActivities").hide() &
+        $(".listUsers").hide();
+});
 
+$(".sectionReservation").hide();
+jQuery(".listMembers").on("click", function () {
+    jQuery(".sectionReservation").show();
+});
 
-jQuery('.reservations').on('click',function(){
-  jQuery('.listReservations').toggle() & $('.listEmployes').hide() & $('.listActivities').hide() & $('.listUsers').hide();
-})
+// jQuery('.listMembers').on('click',function(){
+//     jQuery('.sectionReservation').toggle();
+//   })
 
-jQuery('.listMembers').on('click',function(){
-    jQuery('.sectionReservation').toggle();
-  })
+function member_manage(id) {
+   fetch(`/get-reservations/${id}`).then(reply => reply.text()).then(data => {
+
+   })
+}
 
