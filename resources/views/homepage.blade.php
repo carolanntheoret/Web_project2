3a74ac91c9295a5feb9120376d936a6c3cbfacda
Felynoj <v.aubin.cote@gmail.com>
2023-04-05 15:06:15 -0400

    SE0 homepage pour pull main

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -35,7 +35,7 @@
             </div>
             <div class="news">
                 <div class="mainNews">
-                    <div class="newsPicture" style="background-image: url('{{ $news[0]->image }}')">
+                    <div class="newsPicture" role="img" alt="HIFF - Upcoming Image" style="background-image: url('{{ $news[0]->image }}')">
                         <div class="banner">UPCOMING</div>
                     </div>
                     <p class="fw-semibold fs-2 title-main">{{ $news[0]->title }}</p>
@@ -43,12 +43,12 @@
                     <a href="/news/{{ $news[0]->id }}" class="more">MORE</a>
                 </div>
                 <div class="secondary">
-                    <div class="newsPicture" style="background-image: url('{{ $news[1]->image }}')">
+                    <div class="newsPicture"role="img" alt="HIFF - Movies Image"  style="background-image: url('{{ $news[1]->image }}')">
                         <div class="banner">MOVIES</div>
                     </div>
                     <p class="fw-semibold fs-4 title-sec"><a href="/news/{{ $news[1]->id }}">{{ $news[1]->title }}</a>
                     </p>
-                    <div class="newsPicture" style="background-image: url('{{ $news[2]->image }}'); margin-top: 30px;">
+                    <div class="newsPicture" role="img" alt="HIFF - Award Image" style="background-image: url('{{ $news[2]->image }}'); margin-top: 30px;">
                         <div class="banner">AWARDS</div>
                     </div>
                     <p class="fw-semibold fs-4 title-sec"><a
@@ -59,7 +59,7 @@
 
         <section class="showEvents">
             <div class="eventTop">
-                <div class="image" style="background-image: url('images/tim-cooper-1R7iHPt63Lc-unsplash.jpg')"
+                <div class="image" role="img" alt="HIFF - Event in our festival" style="background-image: url('images/tim-cooper-1R7iHPt63Lc-unsplash.jpg')"
 
                 ></div>
                 <div class="event">
@@ -67,17 +67,17 @@
                     <h3 class="fw-semibold">Feast and Film: A Night of Food and Film</h3>
                     <div class="book-ticket">
                         <p>From $54.99</p>
-                        <a href="/packages" class="fw-semibold">Book ticket <img src="{{ asset('images/arrow.png') }}" alt="" class="book"></a>
+                        <a href="/packages" class="fw-semibold">Book ticket <img src="{{ asset('images/arrow.png') }}" alt="Hiff- arrow to book a ticket" class="book"></a>
                     </div>
                 </div>
-                <div class="image" style="background-image: url('images/samuel-ramos-YfsyWcoI9Ko-unsplash.jpg')"></div>
+                <div class="image" role="img" alt="HIFF - Event in our festival"  style="background-image: url('images/samuel-ramos-YfsyWcoI9Ko-unsplash.jpg')"></div>
                 <div class="event">
                     <p class="fw-semibold" style="margin-top: 18%;">June 4, 2023</p>
                     <h3 class="fw-semibold">HIFF 2023 Closing Night: Awards Ceremony and Screening of "Beyond the
                         Horizon"</h3>
                     <div class="book-ticket">
                         <p>View Packages</p>
-                        <a href="/packages" class="fw-semibold">Our Packages <img src="{{ asset('images/arrow.png') }}" alt="" class="book"></a>
+                        <a href="/packages" class="fw-semibold">Our Packages <img src="{{ asset('images/arrow.png') }}" alt="Hiff- arrow to book a ticket" class="book"></a>
                     </div>
                 </div>
             </div>
@@ -87,20 +87,20 @@
                     <h2 class="fw-semibold">Under the Stars: Exclusive outdoor screenings exclusivity</h2>
                     <div class="book-ticket">
                         <p>View Schedule</p>
-                        <a href="/schedule" class="fw-semibold">Schedule 2023 <img src="{{ asset('images/arrow.png') }}" alt="" class="book"></a>
+                        <a href="/schedule" class="fw-semibold">Schedule 2023 <img src="{{ asset('images/arrow.png') }}" alt="Hiff- arrow to book a ticket" class="book"></a>
                     </div>
                 </div>
-                <div class="image" style="background-image: url('images/stephen-mcfadden-1JOsn6qk8w4-unsplash.jpg')">
+                <div class="image" role="img" alt="HIFF - Event in our festival" style="background-image: url('images/stephen-mcfadden-1JOsn6qk8w4-unsplash.jpg')">
                 </div>
                 <div class="event">
                     <p class="fw-semibold" style="margin-top: 18%;">June 2, 2023</p>
                     <h2 class="fw-semibold">Workshops and Masterclasses: "Directing 101: From Script to Screen"</h2>
                     <div class="book-ticket">
                         <p>From $54.99</p>
-                        <a href="/packages" class="fw-semibold">Book ticket <img src="{{ asset('images/arrow.png') }}" alt="" class="book"></a>
+                        <a href="/packages" class="fw-semibold">Book ticket <img src="{{ asset('images/arrow.png') }}" alt="Hiff- arrow to book a ticket" class="book"></a>
                     </div>
                 </div>
-                <div class="image" style="background-image: url('images/miguel-henriques-RfiBK6Y_upQ-unsplash.jpg')">
+                <div class="image" role="img" alt="HIFF - Event in our festival" style="background-image: url('images/miguel-henriques-RfiBK6Y_upQ-unsplash.jpg')">
                 </div>
         </section>
     </main>
